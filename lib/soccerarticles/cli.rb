3f53a2587013fa8b62f3a7be722f06ba942888e2
Tomas Perez <tomasitopg6@gmail.com>
2020-07-10 02:24:20 +0000

    first commit

---
+++ lib/soccerarticles/cli.rb
@@ -0,0 +1,8 @@
+require "soccerarticles/cli/version"
+
+module Soccerarticles
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/soccerarticles/cli/version.rb
@@ -0,0 +1,5 @@
+module Soccerarticles
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
