653a318aef8627844f2d2ea0fc7b5e876ffd50b7
Tomas Perez <tomasitopg6@gmail.com>
2020-07-23 23:08:06 +0000

    Making application DRY

--- lib/cli.rb
+++ lib/cli.rb
@@ -38,39 +38,48 @@ class CLI
 
     input = gets.strip.to_s
 
-    if input == "1"
-      puts BestPlayers.all[0].name
-      puts BestPlayers.all[0].description
-    elsif input == "2"
-      puts BestPlayers.all[1].name
-      puts BestPlayers.all[1].description
-    elsif input == "3"
-      puts BestPlayers.all[2].name
-      puts BestPlayers.all[2].description
-    elsif input == "4"
-      puts BestPlayers.all[3].name
-      puts BestPlayers.all[3].description
-    elsif input == "5"
-      puts BestPlayers.all[4].name
-      puts BestPlayers.all[4].description
-    elsif input == "6"
-      puts BestPlayers.all[5].name
-      puts BestPlayers.all[5].description
-    elsif input == "7"
-      puts BestPlayers.all[6].name
-      puts BestPlayers.all[6].description
-    elsif input == "8"
-      puts BestPlayers.all[7].name
-      puts BestPlayers.all[7].description
-    elsif input == "9"
-      puts BestPlayers.all[8].name
-      puts BestPlayers.all[8].description
-    elsif input == "10"
-      puts BestPlayers.all[9].name
-      puts BestPlayers.all[9].description
-    else
-      puts "Invalid"
-    end
+    BestPlayers.all.each.with_index(1){
+      |player, index|
+      if input == index.to_s
+        puts "#{player.name}"
+        puts "#{player.description}"
+      end
+    }
+
+
+    # if input == "1"
+    #   puts BestPlayers.all[0].name
+    #   puts BestPlayers.all[0].description
+    # elsif input == "2"
+    #   puts BestPlayers.all[1].name
+    #   puts BestPlayers.all[1].description
+    # elsif input == "3"
+    #   puts BestPlayers.all[2].name
+    #   puts BestPlayers.all[2].description
+    # elsif input == "4"
+    #   puts BestPlayers.all[3].name
+    #   puts BestPlayers.all[3].description
+    # elsif input == "5"
+    #   puts BestPlayers.all[4].name
+    #   puts BestPlayers.all[4].description
+    # elsif input == "6"
+    #   puts BestPlayers.all[5].name
+    #   puts BestPlayers.all[5].description
+    # elsif input == "7"
+    #   puts BestPlayers.all[6].name
+    #   puts BestPlayers.all[6].description
+    # elsif input == "8"
+    #   puts BestPlayers.all[7].name
+    #   puts BestPlayers.all[7].description
+    # elsif input == "9"
+    #   puts BestPlayers.all[8].name
+    #   puts BestPlayers.all[8].description
+    # elsif input == "10"
+    #   puts BestPlayers.all[9].name
+    #   puts BestPlayers.all[9].description
+    # else
+    #   puts "Invalid"
+    # end
   end
 
 
